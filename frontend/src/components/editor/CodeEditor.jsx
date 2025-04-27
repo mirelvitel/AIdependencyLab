c440f2d3fe9ce84224ef9de617c9251afaec25d6
mirelvitel <mirel@vitel.at>
2025-04-27 20:03:44 +0200

    Smaller Height For Code Editor

--- frontend/src/components/editor/CodeEditor.jsx
+++ frontend/src/components/editor/CodeEditor.jsx
@@ -54,7 +54,7 @@ const CodeEditor = ({ task, exerciseId }) => {
 
     return (
         <div className="w-full max-w-4xl mx-auto p-4">
-            <div style={{ height: 700 }}>
+            <div style={{ height: 600 }}>
                 <Editor
                     height="100%"
                     language="java"
